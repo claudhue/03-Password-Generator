43d3206382fc6c77975c92d88e166a2adec87c24
Francisco Goya <degoyafran@gmail.com>
2020-02-07 15:53:16 -0600

    add functions and if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,51 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-var passwordLength = prompt("What is the length of the password?")
-console.log(passwordLength)
-var randomIndex = Math.floor(Math.random()*passwordLength)
-
-var includeLowercase = confirm("Password will include lowercase letters")
-if(includeLowercase == true)
-
-var includeUppercase = confirm("Do you want to include uppercase letters?");
-if(includeUppercase == true)
-
-var includeNumbers = confirm("Do you want to include numbers?");
-if(includeNumbers == true)
-
-var includeSymbols = confirm("Do you want to include symbols?");
-if(includeSymbols == true)
-
 var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numberCharacters = "123456789".split("");
 var symbolCharacters = "!@#$%^&*()=+/-".split("");
+var passwordCharacters;
 
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var passwordLength;
+  do {
+    passwordLength = prompt("Select a password length between 8 and 128?");
+    console.log(passwordLength);
+  } while (passwordLength < 8 || passwordLength > 128);
 
-function writePassword(e) {
-    const value = e.target.value
-    passwordLength.value = value
+  var includeLowercase = confirm("Password will include lowercase letters");
+  if (includeLowercase == true) {
+    passwordCharacters = lowercaseCharacters + passwordCharacters;
+  }
+
+  var includeUppercase = confirm("Do you want to include uppercase letters?");
+  if (includeUppercase == true) {
+    passwordCharacters = uppercaseCharacters + passwordCharacters;
+  }
+
+  var includeNumbers = confirm("Do you want to include numbers?");
+  if (includeNumbers == true) {
+    passwordCharacters = numberCharacters + passwordCharacters;
+  }
+
+  var includeSymbols = confirm("Do you want to include symbols?");
+  if (includeSymbols == true) {
+    passwordCharacters = symbolCharacters + passwordCharacters;
+  }
+
+  for (let i = 0; i < passwordLength - 1; i++) {
+    Math.floor(Math.random() * passwordCharacters.length);
+  }
+  var password = passwordCharacters[i];
+  password = password + passwordCharacters[i];
 }
+
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+generateBtn.addEventListener("click", generatePassword);
+
+// Assignment Code
 // Write password to the #password input
 // Add event listener to generate button
 //if user allows uppercase
-  //generate random uppercase letters
-  //append some uppercase letters to the textarea (or wherever)
+//generate random uppercase letters
+//append some uppercase letters to the textarea (or wherever)
